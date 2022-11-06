fedebae84c5731ca48cda59f99da38e3dc5d1182
manioui <117488357+manioui@users.noreply.github.com>
2022-11-06 10:13:28 +0100

    Update ml.py

--- ml.py
+++ ml.py
@@ -13,10 +13,10 @@ from collections import deque
 import warnings
 warnings.filterwarnings("ignore")
 
-
+#Cette classe est utilisée dans Ids_app.py pour la détection des DDoS.
 class MachineLearningAlgo:
     def __init__(self):
-        # Hybrid
+        # Initialisation des classificateurs et aussi Hybrid
         clf1 = svm.SVC(kernel="rbf", probability=True,
                        random_state=0, gamma=0.001, C=100)
         clf2 = RandomForestClassifier(
@@ -27,14 +27,17 @@ class MachineLearningAlgo:
         self.clf = EnsembleVoteClassifier(clfs=[clf1, clf2, clf3],
                                           weights=[1, 1, 1], voting='soft')
         print("Using Hybrid Algo (SVM + RF + MLP) ")
-
+	#chargement des données 
         X_train = pd.read_csv('result.csv')
         y_train = X_train["type"]
         del X_train["type"]
         X_train.iloc[:] = minmax_scale(X_train.iloc[:])
+	#entraîner notre modèle final
         self.clf.fit(X_train, y_train.values.ravel())
 
     def classify(self, data):
+	#faire une prédiction
         prediction = self.clf.predict(data)
+	#Montrer la prédiction prévues
         print("prediction result ", prediction)
         return prediction
